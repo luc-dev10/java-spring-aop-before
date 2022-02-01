ed0de82d2cff9b048c7575261f50e61493cfc3ee
luc-dev10 <luc.dev1008@gmail.com>
2022-01-31 20:46:17 -0500

    add bean and use in controller class

--- src/main/java/com/aop/SpringConfiguration.java
+++ src/main/java/com/aop/SpringConfiguration.java
@@ -2,6 +2,8 @@ package com.aop;
 
 import com.aop.service.AccountService;
 import com.aop.service.AccountServiceImpl;
+import com.aop.service.MembershipService;
+import com.aop.service.MembershipServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,11 @@ public class SpringConfiguration {
     @Bean
     public AccountService accountService() {
         return new AccountServiceImpl();
+    }
+
+    @Bean
+    public MembershipService membershipService() {
+        return new MembershipServiceImpl();
     }
 
 }
---
+++ src/main/java/com/aop/controller/AppController.java
@@ -0,0 +1,47 @@
+package com.aop.controller;
+
+import com.aop.SpringConfiguration;
+import com.aop.entity.Account;
+import com.aop.entity.Membership;
+import com.aop.service.AccountService;
+import com.aop.service.MembershipService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppController {
+
+    // simulates the controller class
+    public static void main(String[] args) {
+
+        // read configuration
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+
+        AccountService accountService = context.getBean("accountService", AccountService.class);
+        MembershipService membershipService = context.getBean("membershipService", MembershipService.class);
+
+        // call business method
+        Account account = new Account();
+        account.setId(1);
+        account.setFirstName("Lucio");
+        account.setLastName("Zhao");
+        account.setEmail("lucio.zhao@email.com");
+
+        // save account
+        accountService.saveAccount(account);
+
+        // AOP
+        System.out.println(accountService.isServiceRunning());
+
+        // create membership
+        Membership membership = new Membership();
+        membership.setType(Membership.MEMBERSHIP_TYPE.FREE);
+        System.out.println(membership.getId());
+        // AOP
+        System.out.println(accountService.isServiceRunning());
+
+        // print account
+
+        // close
+        context.close();
+
+    }
+}
